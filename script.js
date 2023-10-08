59e7b64f221abdd5f9d49d289cf26bd09d15185d
Md. Nishat Raihan Khan Nijhum <mdnishatraihankhan@gmail.com>
2023-10-08 06:41:36 +0600

    added html, css, js, img files

---
+++ script.js
@@ -0,0 +1,9 @@
+window.onload = function () {
+  if (window.innerWidth >= 0 && window.innerWidth <= 319) {
+    alert(
+      "You need a larger screen to see the content, please upgrade your device :)"
+    );
+  } else {
+    alert("As-salamu alaykum / Hello👋");
+  }
+};
