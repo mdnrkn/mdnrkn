2a0d175b2b46a5fbf7a6ecc65f519e909e9222dd
Md. Nishat Raihan Khan Nijhum <mdnishatraihankhan@gmail.com>
2023-12-16 15:42:40 +0600

    deleted previous code

--- script.js
+++
@@ -1,9 +0,0 @@
-window.onload = function () {
-  if (window.innerWidth >= 0 && window.innerWidth <= 319) {
-    alert(
-      "You need a larger screen to see the content, please upgrade your device :)"
-    );
-  } else {
-    alert("As-salamu alaykum / Hello 👋");
-  }
-};
